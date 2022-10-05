aaa105535d410347efe877b2de96d83513834bf5
Puiu Dumitru99 <puiudumitru99@gmail.com>
2022-10-05 10:50:09 +0300

    Delete Read

--- script.js
+++ script.js
@@ -6,7 +6,10 @@ function hide()
 function unhide()
 {
  document.getElementById("divlogin").style.display="block";
+ document.getElementById("regis").style.display="none";
 }
-
-
-
+function unhideregistre()
+{
+document.getElementById("divlogin").style.display="none";
+ document.getElementById("regis").style.display="block";
+}
